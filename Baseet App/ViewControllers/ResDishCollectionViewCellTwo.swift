6e67a13f0199c11df624c7adec0a9101d07c427a
Subay <subay421@gmail.com>
2022-08-08 00:38:43 +0530

    Go to cart flow added

--- Baseet App/ViewControllers/ResDishCollectionViewCellTwo.swift
+++ Baseet App/ViewControllers/ResDishCollectionViewCellTwo.swift
@@ -9,4 +9,37 @@ import UIKit
 
 class ResDishCollectionViewCellTwo: UICollectionViewCell {
     
+    @IBOutlet weak var buttonVegOrNV: UIButton!
+    @IBOutlet weak var buttonAdd: UIButton!
+    @IBOutlet weak var itemName: UILabel!
+    @IBOutlet weak var itemImage: UIImageView!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var itemCountLabel: UILabel!
+    var itemCount = 1
+    @IBOutlet weak var countView: UIView!
+    var itemAdded:((Bool)->())?
+
+    @IBAction func actionAdd(_ sender: Any) {
+        self.buttonAdd.isHidden = true
+        self.countView.isHidden = false
+        self.itemAdded?(true)
+    }
+    
+    @IBAction func actionIncrease(_ sender: Any) {
+        itemCount = itemCount + 1
+        self.itemCountLabel.text = "\(itemCount)"
+    }
+    
+    @IBAction func actionReduce(_ sender: Any) {
+        if itemCount == 1 {
+        itemCount = 1
+            self.itemAdded?(false)
+            self.buttonAdd.isHidden = false
+            self.countView.isHidden = true
+        } else {
+            itemCount = itemCount - 1
+            self.itemCountLabel.text = "\(itemCount)"
+        }
+    }
+    
 }
--- Baseet App/ViewControllers/RestarentDishViewController.swift
+++ Baseet App/ViewControllers/RestarentDishViewController.swift
@@ -13,12 +13,15 @@ class RestarentDishViewController: UIViewController {
     @IBOutlet weak var timingLabel: UILabel!
     @IBOutlet weak var restaurantName: UILabel!
     
+    @IBOutlet weak var buttonGoToCart: UIButton!
     @IBOutlet weak var restaurantAddress: UILabel!
     var menu_vc1: RecipeDetailsVC!
-
+    
     @IBOutlet weak var resDishCV: UICollectionView!
     
     @IBOutlet weak var resDishTB: UICollectionView!
+    
+    @IBOutlet weak var collectionViewBottomConstraint: NSLayoutConstraint!
     
     var restarentDishViewControllerVM: RestarentDishViewControllerVM?
     
@@ -28,12 +31,12 @@ class RestarentDishViewController: UIViewController {
         
         menu_vc1 = self.storyboard?.instantiateViewController(withIdentifier: "RecipeDetailsVC") as? RecipeDetailsVC
         setupValues()
-//        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToGesture))
-//        swipeRight.direction = UISwipeGestureRecognizer.Direction.right
-//        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToGesture))
-//        swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
-//        self.view.addGestureRecognizer(swipeLeft)
-//        self.view.addGestureRecognizer(swipeRight)
+        //        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToGesture))
+        //        swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+        //        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToGesture))
+        //        swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
+        //        self.view.addGestureRecognizer(swipeLeft)
+        //        self.view.addGestureRecognizer(swipeRight)
     }
     
     override func viewWillAppear(_ animated: Bool)
@@ -69,24 +72,31 @@ class RestarentDishViewController: UIViewController {
         self.dismiss(animated: true,completion: nil)
     }
     
+    @IBAction func actionGoToBasket(_ sender: Any) {
+        let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(identifier: "RestaurentFoodPicksVC") as! RestaurentFoodPicksVC
+        vc.modalPresentationStyle = .fullScreen
+        self.present(vc, animated: true, completion: nil)
+    }
+    
     func setupValues() {
         self.logoImage.loadImageUsingURL(self.restarentDishViewControllerVM?.shopDetailsModel?.applogo ?? "")
         self.restaurantName.text = self.restarentDishViewControllerVM?.shopDetailsModel?.name
         self.restaurantAddress.text = self.restarentDishViewControllerVM?.shopDetailsModel?.address
-
+        self.buttonGoToCart.isHidden  = true
     }
-
+    
 }
 
 extension RestarentDishViewController:UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout
- {
+{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if collectionView == self.resDishCV
-           {
-               return 8
-           }
-       
+        {
             return 8
+        }
+        
+        return 8
         
     }
     
@@ -102,44 +112,52 @@ extension RestarentDishViewController:UICollectionViewDelegate,UICollectionViewD
             let cellA = resDishCV.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ResDishCollectionViewCell
             
             
-
-                
-                return cellA
+            
+            
+            return cellA
         }
         
-        else  
+        else
         {
             let cellC = resDishTB.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ResDishCollectionViewCellTwo
             
-            
-                return cellC
+            cellC.buttonAdd.layer.cornerRadius = 10
+            cellC.itemAdded  = { bool in
+                DispatchQueue.main.async {
+                    self.buttonGoToCart.isHidden = !bool
+                    if bool == false {
+                        self.collectionViewBottomConstraint.constant = 66
+                    } else {
+                        self.collectionViewBottomConstraint.constant = 130
+                    }
+                }
+            }
+            return cellC
         }
-
-           
     }
     
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//           let padding: CGFloat =  50
-//           let collectionViewSize = collectionView.frame.size.width - padding
-//
-//           return CGSize(width: collectionViewSize/2, height: collectionViewSize/2)
-//       }
+    //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    //           let padding: CGFloat =  50
+    //           let collectionViewSize = collectionView.frame.size.width - padding
+    //
+    //           return CGSize(width: collectionViewSize/2, height: collectionViewSize/2)
+    //       }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let yourWidth = collectionView.bounds.width/3.0
         let yourHeight = yourWidth
-
+        
         return CGSize(width: yourWidth, height: yourHeight)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets.zero
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 0
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 0
     }
